9670d7bf97363a8e740bdef891d8ce62f8e94d5e
ZIEGLER Aurelie (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / EQ ESC-VEN MUL) <8301681s@commun.ad.sncf.fr>
2023-07-20 09:04:58 +0200

    files java

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,49 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+
+
+
+import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-    @ResponseBody
-    public Doctor doctor() {
+ 
+/**
+ * @param number
+ * @return
+ */
+@GetMapping("/doctor/{number}")
+@ResponseBody
+public Doctor doctor (@PathVariable int number){
+    Doctor doctor = new Doctor();
 
-        return null;
+
+    if(number == 13) {
+        doctor.setName("jodie Whittaker");
+        doctor.setNumber( 13);
+        return doctor;
+        
+    }else if (number > 0 && number < 13){
+        throw new ResponseStatusException(HttpStatus.SEE_OTHER,"303");
+    }
+    else{
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,"404");
     }
 }
+
+}
+
+
+
+
